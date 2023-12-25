26532ba17a5bbd3c02affa7f208f9c48003bfe20
B <59208263+venfo9@users.noreply.github.com>
2023-12-25 16:20:53 +0200

    Add files via upload

--- tests/Tests2.spec.ts
+++ tests/Tests2.spec.ts
@@ -1,26 +1,24 @@
-const config = require('../playwright.config.js');
+const config = require('../playwright.config');
+import { test } from '../fixtures/fixturePagesTS';
 
-import {test, expect} from '@playwright/test'
+import {expect} from '@playwright/test'
 import {Login} from '../pages/LoginTS'
 import {ChooseGoods} from '../pages/ChooseGoodsTS'
 import {Cart} from '../pages/CartTS'
 
 
-test('Compare good amount on good list page and in the cart page', async ({page}) => {
+test('Compare good amount on good list page and in the cart page', async ({cart,chooseGoods,login}) => {
     //Login
-    const login = new Login(page)
     await login.gotoLogin()
     await login.enterCredentials(config.email, config.password)
 
     //ChooseGoods
-    const chooseGoods = new ChooseGoods(page)
     await chooseGoods.enterSearchWord("t-shirt")
     await chooseGoods.switchToGrid()
     let amountFromChooseGoods = await chooseGoods.addToCart();
     await chooseGoods.gotoCart()
 
     //Cart
-    const cart = new Cart(page)
     let amountFromCart = await cart.Cart();
 
 
